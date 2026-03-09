e044c41d279e1d9ce1c7d7700b4557a249430e41
Nick Hayward <nickhayward.nz@gmail.com>
2026-03-09 13:41:05 +1300

    Fix light blue globe - use stable pastel-bright Earth textures

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -5,9 +5,9 @@ import type { City } from '../data/cities'
 import { latLonToVector3 } from '../lib/geo'
 import { getCityTimeInfo } from '../lib/time'
 
-const DAY_MAP_URL = 'https://unpkg.com/three@0.160.0/examples/textures/planets/earth_atmos_2048.jpg'
-const NIGHT_MAP_URL = 'https://unpkg.com/three@0.160.0/examples/textures/planets/earth_lights_2048.jpg'
-const SPEC_MAP_URL = 'https://unpkg.com/three@0.160.0/examples/textures/planets/earth_specular_2048.jpg'
+const DAY_MAP_URL = 'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_atmos_2048.jpg'
+const NIGHT_MAP_URL = 'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_lights_2048.jpg'
+const SPEC_MAP_URL = 'https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/planets/earth_specular_2048.jpg'
 
 const AUTO_ROTATE_Y_DEG_PER_FRAME = 0.12
 const DOT_UPDATE_INTERVAL_MS = 120000
@@ -60,8 +60,8 @@ export function Globe({ now, cities }: Props) {
     const group = new THREE.Group()
     scene.add(group)
 
-    scene.add(new THREE.AmbientLight(0xffffff, 0.8))
-    const sun = new THREE.DirectionalLight(0xffffff, 1.8)
+    scene.add(new THREE.AmbientLight(0xffffff, 1.0))
+    const sun = new THREE.DirectionalLight(0xffffff, 2.0)
     sun.position.set(5, 3, 5)
     scene.add(sun)
 
@@ -72,7 +72,7 @@ export function Globe({ now, cities }: Props) {
     const earthMat = new THREE.MeshPhongMaterial({
       color: new THREE.Color(0xffffff),
       emissive: new THREE.Color(0x444444),
-      emissiveIntensity: 0.8,
+      emissiveIntensity: 1.0,
       specular: new THREE.Color(0x444444),
       shininess: 5,
     })
@@ -94,15 +94,15 @@ export function Globe({ now, cities }: Props) {
       if (!ctx) return sourceTex as THREE.CanvasTexture
       ctx.drawImage(img, 0, 0)
       ctx.globalCompositeOperation = 'source-over'
-      ctx.fillStyle = 'rgba(255,255,255,0.5)'
+      ctx.fillStyle = 'rgba(255,255,255,0.6)'
       ctx.fillRect(0, 0, w, h)
       ctx.globalCompositeOperation = 'saturation'
-      ctx.fillStyle = 'rgba(100,150,255,0.4)'
+      ctx.fillStyle = 'rgba(100,150,255,0.5)'
       ctx.fillRect(0, 0, w, h)
       const pastelTexture = new THREE.CanvasTexture(canvas)
       pastelTexture.needsUpdate = true
       // eslint-disable-next-line no-console
-      console.log('Pastel texture applied')
+      console.log('Pastel texture applied successfully')
       return pastelTexture
     }
 
@@ -118,7 +118,7 @@ export function Globe({ now, cities }: Props) {
       mat.map = dayTexture
       mat.emissiveMap = nightTexture ?? null
       mat.emissive = new THREE.Color(0x444444)
-      mat.emissiveIntensity = 0.8
+      mat.emissiveIntensity = 1.0
       mat.specularMap = specularTexture ?? null
       mat.specular = new THREE.Color(0x444444)
       mat.shininess = 5
@@ -142,7 +142,7 @@ export function Globe({ now, cities }: Props) {
         // eslint-disable-next-line no-console
         console.error('Day map load failed:', err)
         const mat = earth.material as THREE.MeshPhongMaterial
-        mat.color = new THREE.Color(0x88ddff)
+        mat.color = new THREE.Color(0xaaddff)
         // eslint-disable-next-line no-console
         console.log('Globe using fallback pastel blue (day texture failed)')
       },
